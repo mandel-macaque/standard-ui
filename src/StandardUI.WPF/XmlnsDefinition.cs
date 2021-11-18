532e3388fef135e86dafbd44cfd41751f5bc70f2
Bret Johnson <bret.johnson@microsoft.com>
2021-11-17 23:18:22 -0500

    Add WPF namespaces with| URI

---
+++ src/StandardUI.WPF/XmlnsDefinition.cs
@@ -0,0 +1,6 @@
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/standard-ui/2022", "Microsoft.StandardUI.Wpf")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/standard-ui/2022", "Microsoft.StandardUI.Wpf.Controls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/standard-ui/2022", "Microsoft.StandardUI.Wpf.Media")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/standard-ui/2022", "Microsoft.StandardUI.Wpf.Shapes")]
