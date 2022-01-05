7072ff69265286cc8a2352eb9586e9e821adfec7
Bret Johnson <bret.johnson@microsoft.com>
2022-01-05 11:27:43 -0500

    Updated generated WPF source

--- src/StandardUI.WPF/generated/Media/PathGeometry.cs
+++ src/StandardUI.WPF/generated/Media/PathGeometry.cs
@@ -1,6 +1,5 @@
 // This file is generated from IPathGeometry.cs. Update the source file to change its contents.
 
-using System.Collections.Generic;
 using Microsoft.StandardUI.Media;
 using DependencyProperty = System.Windows.DependencyProperty;
 
@@ -8,14 +7,19 @@ namespace Microsoft.StandardUI.Wpf.Media
 {
     public class PathGeometry : Geometry, IPathGeometry
     {
-        public static readonly DependencyProperty FiguresProperty = PropertyUtils.Register(nameof(Figures), typeof(IEnumerable<IPathFigure>), typeof(PathGeometry), null);
+        public static readonly DependencyProperty FiguresProperty = PropertyUtils.Register(nameof(Figures), typeof(UICollection<IPathFigure>), typeof(PathGeometry), null);
         public static readonly DependencyProperty FillRuleProperty = PropertyUtils.Register(nameof(FillRule), typeof(FillRule), typeof(PathGeometry), FillRule.EvenOdd);
         
-        public IEnumerable<IPathFigure> Figures
+        private UICollection<IPathFigure> _figures;
+        
+        public PathGeometry()
         {
-            get => (IEnumerable<IPathFigure>) GetValue(FiguresProperty);
-            set => SetValue(FiguresProperty, value);
+            _figures = new UICollection<IPathFigure>(this);
+            SetValue(FiguresProperty, _figures);
         }
+        
+        public UICollection<IPathFigure> Figures => _figures;
+        IUICollection<IPathFigure> IPathGeometry.Figures => Figures;
         
         public FillRule FillRule
         {
