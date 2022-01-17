bf06fec59436be0109c75dbeba472ff112c3f7d4
Bret Johnson <bret.johnson@microsoft.com>
2022-01-17 09:40:01 -0500

    Add CanvasHelperExtensions, similar to GridHelperExtensions

---
+++ src/StandardUI/Controls/CanvasHelperExtensions.cs
@@ -0,0 +1,19 @@
+using static Microsoft.StandardUI.FactoryStatics;
+
+namespace Microsoft.StandardUI.Controls
+{
+    public static class CanvasHelperExtensions
+    {
+        public static T _<T>(this T canvas, params IUIElement[] children) where T : ICanvas =>
+            canvas.Children(children);
+
+        public static T Add<T>(this T canvas, double left, double top, IUIElement child) where T : ICanvas
+        {
+            canvas.Children.Add(child.CanvasLeft(left).CanvasTop(top));
+            return canvas;
+        }
+
+        public static T CanvasPosition<T>(this T uiElement, double left, double top) where T : IUIElement =>
+            uiElement.CanvasLeft(left).CanvasTop(top);
+    }
+}
