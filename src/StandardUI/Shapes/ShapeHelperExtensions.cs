4cbb799dd2764b58f55b2399dbbcc00649fe5279
Bret Johnson <bret.johnson@microsoft.com>
2022-01-18 09:35:05 -0500

    Add Fill/Stroke helpers to set directly via color

---
+++ src/StandardUI/Shapes/ShapeHelperExtensions.cs
@@ -0,0 +1,13 @@
+using static Microsoft.StandardUI.FactoryStatics;
+
+namespace Microsoft.StandardUI.Shapes
+{
+    public static class ShapeHelperExtensions
+    {
+        public static T Fill<T>(this T shape, Color color) where T : IShape =>
+            shape.Fill(SolidColorBrush(color));
+
+        public static T Stroke<T>(this T shape, Color color) where T : IShape =>
+            shape.Stroke(SolidColorBrush(color));
+    }
+}
