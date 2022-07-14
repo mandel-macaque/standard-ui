723dc048f7eb154c307a0c9df4fff79b1617d814
Bret Johnson <bret.johnson@microsoft.com>
2022-07-14 13:59:57 -0400

    Rename CreatorNotInitializedException -> FactoryNotInitializedException

---
+++ src/Microsoft.StandardUI/FactoryNotInitializedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microsoft.StandardUI
+{
+    public class FactoryNotInitializedException : Exception
+    {
+        public FactoryNotInitializedException(string controlLibraryName) : base($"{controlLibraryName} factory hasn't been initialized yet. Call {controlLibraryName}Library.Initialize()")
+        {
+        }
+    }
+}
