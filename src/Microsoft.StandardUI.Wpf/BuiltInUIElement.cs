59442af800a199173f2a5a182601e9c75e91a89f
Bret Johnson <bret.johnson@microsoft.com>
2022-09-18 23:23:20 -0400

    Generate parts of BuildInUIElement and StandardControl, for WPF

--- src/Microsoft.StandardUI.Wpf/BuiltInUIElement.cs
+++ src/Microsoft.StandardUI.Wpf/BuiltInUIElement.cs
@@ -1,62 +1,14 @@
-using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
 
 namespace Microsoft.StandardUI.Wpf
 {
     /// <summary>
-    /// This is the base for predefined Standard UI controls. 
+    /// This is the base for predefined Standard UI controls.
     /// </summary>
-    public class BuiltInUIElement : FrameworkElement, IUIElement, ILogicalParent
+    public partial class BuiltInUIElement : FrameworkElement, IUIElement, ILogicalParent
     {
         private StandardUIFrameworkElementHelper _helper = new();
-
-        void IUIElement.Measure(Size availableSize)
-        {
-            Measure(availableSize.ToWpfSize());
-        }
-
-        void IUIElement.Arrange(Rect finalRect)
-        {
-            Arrange(finalRect.ToWpfRect());
-        }
-
-        Size IUIElement.DesiredSize => SizeExtensions.ToStandardUISize(DesiredSize);
-
-        double IUIElement.ActualX => throw new NotImplementedException();
-
-        double IUIElement.ActualY => throw new NotImplementedException();
-
-        Thickness IUIElement.Margin
-        {
-            get => Margin.ToStandardUIThickness();
-            set => Margin = value.ToWpfThickness();
-        }
-
-        HorizontalAlignment IUIElement.HorizontalAlignment
-        {
-            get => HorizontalAlignment.ToStandardUIHorizontalAlignment();
-            set => HorizontalAlignment = value.ToWpfHorizontalAlignment();
-        }
-
-        VerticalAlignment IUIElement.VerticalAlignment
-        {
-            get => VerticalAlignment.ToStandardUIVerticalAlignment();
-            set => VerticalAlignment = value.ToWpfVerticalAlignment();
-        }
-
-        FlowDirection IUIElement.FlowDirection
-        {
-            get => FlowDirection.ToStandardUIFlowDirection();
-            set => FlowDirection = value.ToWpfFlowDirection();
-        }
-
-        // TODO: Error if appropriate when set to Visibility.Hidden
-        bool IUIElement.Visible
-        {
-            get => Visibility != Visibility.Collapsed;
-            set => Visibility = value ? Visibility.Visible : Visibility.Collapsed;
-        }
 
         protected override void OnRender(DrawingContext drawingContextWpf)
         {
@@ -90,11 +42,6 @@ namespace Microsoft.StandardUI.Wpf
         public virtual void Draw(IDrawingContext visualizer)
         {
         }
-
-        object? IUIObject.GetValue(IUIProperty property) => GetValue(((UIProperty) property).DependencyProperty);
-        object? IUIObject.ReadLocalValue(IUIProperty property) => ReadLocalValue(((UIProperty)property).DependencyProperty);
-        void IUIObject.SetValue(IUIProperty property, object? value) => SetValue(((UIProperty)property).DependencyProperty, value);
-        void IUIObject.ClearValue(IUIProperty property) => ClearValue(((UIProperty)property).DependencyProperty);
 
         void ILogicalParent.AddLogicalChild(object child) => AddLogicalChild(child);
         void ILogicalParent.RemoveLogicalChild(object child) => RemoveLogicalChild(child);
