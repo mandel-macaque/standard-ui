1cb743f571c4c87de02b7b7741153bcd1ae5844a
Ali Hamie <ali_leb29@hotmail.com>
2022-09-19 09:40:43 -0700

    added Blazor project (#14)
    * added Blazor project
    * Added a Graph
    * Move Blazor sample to host-apps
    Co-authored-by: Bret Johnson <bret.johnson@microsoft.com>

---
+++ src/StandardUI.VisualEnvironment.Blazor/BlazorVisualEnvironment.cs
@@ -0,0 +1,31 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas.Canvas2D;
+using Microsoft.StandardUI;
+using System;
+namespace StandardUI.VisualEnvironment.Blazor
+{
+    public class BlazorVisualEnvironment : IVisualEnvironment
+    {
+        private Canvas2DContext context;
+
+        public BlazorVisualEnvironment(Canvas2DContext canvas)
+        {
+            context = canvas;
+        }
+
+        public IDrawingContext CreateDrawingContext(in Rect cullingRect)
+        {
+            return new BlazorDrawingContext(context);
+        }
+
+        public IVisualHostControl CreateHostControl(object? arg1 = null, object? arg2 = null, object? arg3 = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderToBuffer(IVisual visual, IntPtr pixels, int width, int height, int rowBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
