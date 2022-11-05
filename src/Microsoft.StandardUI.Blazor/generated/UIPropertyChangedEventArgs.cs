25847dccf946fa189ced8565c6102f4652690fb4
Bret Johnson <bret.johnson@microsoft.com>
2022-11-04 20:54:23 -0400

    Fix other build errors

--- src/Microsoft.StandardUI.Blazor/generated/UIPropertyChangedEventArgs.cs
+++
@@ -1,8 +0,0 @@
-// This file is generated from IUIPropertyChangedEventArgs.cs. Update the source file to change its contents.
-
-namespace Microsoft.StandardUI.Blazor
-{
-    public class UIPropertyChangedEventArgs : IUIPropertyChangedEventArgs
-    {
-    }
-}
--- src/Microsoft.StandardUI.Blazor/generated/UIPropertyMetadata.cs
+++
@@ -1,8 +0,0 @@
-// This file is generated from IUIPropertyMetadata.cs. Update the source file to change its contents.
-
-namespace Microsoft.StandardUI.Blazor
-{
-    public class UIPropertyMetadata : IUIPropertyMetadata
-    {
-    }
-}
